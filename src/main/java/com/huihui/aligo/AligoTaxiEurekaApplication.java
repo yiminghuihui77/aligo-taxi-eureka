a3f677a2826d2fa7b3e484ffa6688c1ded65f2e3
尹明辉 <yinminghui343@hellobike.com>
2021-04-25 16:00:43 +0800

    eureka Server搭建

---
+++ src/main/java/com/huihui/aligo/AligoTaxiEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.huihui.aligo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AligoTaxiEurekaApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( AligoTaxiEurekaApplication.class );
+
+    public static void main( String[] args ) {
+        SpringApplication.run( AligoTaxiEurekaApplication.class, args );
+        LOGGER.info( "eureka server started success..." );
+    }
+
+}
